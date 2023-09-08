b61ce0f9453a823f301445074c17f3f66553a9ec
adrian13508 <adrian.kurowski@pm.me>
2023-09-08 23:12:07 +0200

    consumers changed

--- MeetingApp/api/routing.py
+++ MeetingApp/api/routing.py
@@ -2,9 +2,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 # import app.routing
 from django.urls import re_path
 #from api.consumers import VideoConsumer
-from api.consumers import TextRoomConsumer
+from api.consumers import TextRoomConsumer, VideoConsumer
 
 websocket_urlpatterns = [
-    #re_path(r'^ws/(?P<room_name>[^/]+)/$', VideoConsumer.as_asgi()),
-    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+    re_path(r'^ws/call/(?P<room_name>[^/]+)/$', VideoConsumer.as_asgi()),
+    #re_path(r'^ws/call/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
 ]
