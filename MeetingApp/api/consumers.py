a1db970156980511aa3649312c3020edc4db3330
adrian13508 <adrian.kurowski@pm.me>
2023-09-12 22:11:03 +0200

    new

--- MeetingApp/api/consumers.py
+++ MeetingApp/api/consumers.py
@@ -1,61 +1,117 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 
-class VideoConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'video_{self.room_name}'
+class VideoConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
-        # Join the room
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    async def disconnect(self, close_code):
-        # Leave the room
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        if data.get('signalData'):
-            # Handle signaling data (SDP or ICE candidates)
-            signal_data = data['signalData']
-            await self.send_signal(signal_data)
-
-        if data.get('acceptCall'):
-            # Handle call acceptance
-            await self.send_acceptance(data['callerID'], data['signalData'])
-
-    async def send_signal(self, signal_data):
-        # Forward signaling data to other clients in the room
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'forward_signal',
-                'signal_data': signal_data,
-                'sender_channel_name': self.channel_name
+        # response to client, that we are connected.
+        self.send(text_data=json.dumps({
+            'type': 'connection',
+            'data': {
+                'message': "Connected"
             }
-        )
-
-    async def forward_signal(self, event):
-        # Forward signaling data to the recipient client
-        sender_channel_name = event['sender_channel_name']
-        signal_data = event['signal_data']
-
-        await self.send(text_data=json.dumps({
-            'signalData': signal_data,
-            'userFrom': sender_channel_name == self.channel_name
         }))
 
-    async def send_acceptance(self, caller_id, signal_data):
-        # Send call acceptance signal to the caller
-        await self.send(text_data=json.dumps({
-            'callAccepted': True,
-            'signalData': signal_data,
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.my_name,
+            self.channel_name
+        )
+
+    # Receive message from client WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        # print(text_data_json)
+
+        eventType = text_data_json['type']
+
+        if eventType == 'login':
+            name = text_data_json['data']['name']
+
+            # we will use this as room name as well
+            self.my_name = name
+
+            # Join room
+            async_to_sync(self.channel_layer.group_add)(
+                self.my_name,
+                self.channel_name
+            )
+        
+        if eventType == 'call':
+            name = text_data_json['data']['name']
+            print(self.my_name, "is calling", name);
+            # print(text_data_json)
+
+
+            # to notify the callee we sent an event to the group name
+            # and their's groun name is the name
+            async_to_sync(self.channel_layer.group_send)(
+                name,
+                {
+                    'type': 'call_received',
+                    'data': {
+                        'caller': self.my_name,
+                        'rtcMessage': text_data_json['data']['rtcMessage']
+                    }
+                }
+            )
+
+        if eventType == 'answer_call':
+            # has received call from someone now notify the calling user
+            # we can notify to the group with the caller name
+            
+            caller = text_data_json['data']['caller']
+            # print(self.my_name, "is answering", caller, "calls.")
+
+            async_to_sync(self.channel_layer.group_send)(
+                caller,
+                {
+                    'type': 'call_answered',
+                    'data': {
+                        'rtcMessage': text_data_json['data']['rtcMessage']
+                    }
+                }
+            )
+
+        if eventType == 'ICEcandidate':
+
+            user = text_data_json['data']['user']
+
+            async_to_sync(self.channel_layer.group_send)(
+                user,
+                {
+                    'type': 'ICEcandidate',
+                    'data': {
+                        'rtcMessage': text_data_json['data']['rtcMessage']
+                    }
+                }
+            )
+
+    def call_received(self, event):
+
+        # print(event)
+        print('Call received by ', self.my_name )
+        self.send(text_data=json.dumps({
+            'type': 'call_received',
+            'data': event['data']
+        }))
+
+
+    def call_answered(self, event):
+
+        # print(event)
+        print(self.my_name, "'s call answered")
+        self.send(text_data=json.dumps({
+            'type': 'call_answered',
+            'data': event['data']
+        }))
+
+
+    def ICEcandidate(self, event):
+        self.send(text_data=json.dumps({
+            'type': 'ICEcandidate',
+            'data': event['data']
         }))
--- MeetingApp/api/routing.py
+++ MeetingApp/api/routing.py
@@ -5,5 +5,6 @@ from django.urls import re_path
 from api.consumers import VideoConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/call/(?P<room_name>[^/]+)/$', VideoConsumer.as_asgi()),
+    # re_path(r'^ws/call/(?P<room_name>[^/]+)/$', VideoConsumer.as_asgi()),
+    re_path(r'^ws/call/', VideoConsumer.as_asgi())
 ]
