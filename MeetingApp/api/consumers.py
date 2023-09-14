dc1a5115f207bf63ea1565dcca0d8106514bf36b
adrian13508 <adrian.kurowski@pm.me>
2023-09-14 22:54:53 +0200

    consumer changes

--- MeetingApp/api/consumers.py
+++ MeetingApp/api/consumers.py
@@ -1,117 +1,98 @@
+# consumers.py
+
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
 
-class VideoConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class VideoConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"call_{self.room_name}"
 
-        # response to client, that we are connected.
-        self.send(text_data=json.dumps({
-            'type': 'connection',
-            'data': {
-                'message': "Connected"
-            }
-        }))
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.my_name,
+        # Join the room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from client WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        # print(text_data_json)
+        await self.accept()
 
-        eventType = text_data_json['type']
+    async def disconnect(self, close_code):
+        # Leave the room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-        if eventType == 'login':
-            name = text_data_json['data']['name']
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_type = data['type']
 
-            # we will use this as room name as well
-            self.my_name = name
-
-            # Join room
-            async_to_sync(self.channel_layer.group_add)(
-                self.my_name,
-                self.channel_name
-            )
-        
-        if eventType == 'call':
-            name = text_data_json['data']['name']
-            print(self.my_name, "is calling", name);
-            # print(text_data_json)
-
-
-            # to notify the callee we sent an event to the group name
-            # and their's groun name is the name
-            async_to_sync(self.channel_layer.group_send)(
-                name,
+        if message_type == 'join_room':
+            await self.channel_layer.group_send(
+                self.room_group_name,
                 {
-                    'type': 'call_received',
-                    'data': {
-                        'caller': self.my_name,
-                        'rtcMessage': text_data_json['data']['rtcMessage']
-                    }
+                    'type': 'join_room',
+                    'username': data['username'],
+                }
+            )
+        elif message_type == 'offer':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'offer',
+                    'offer': data['offer'],
+                }
+            )
+        elif message_type == 'answer':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'answer',
+                    'answer': data['answer'],
+                }
+            )
+        elif message_type == 'ice_candidate':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'ice_candidate',
+                    'candidate': data['candidate'],
                 }
             )
 
-        if eventType == 'answer_call':
-            # has received call from someone now notify the calling user
-            # we can notify to the group with the caller name
-            
-            caller = text_data_json['data']['caller']
-            # print(self.my_name, "is answering", caller, "calls.")
+    async def join_room(self, event):
+        username = event['username']
 
-            async_to_sync(self.channel_layer.group_send)(
-                caller,
-                {
-                    'type': 'call_answered',
-                    'data': {
-                        'rtcMessage': text_data_json['data']['rtcMessage']
-                    }
-                }
-            )
-
-        if eventType == 'ICEcandidate':
-
-            user = text_data_json['data']['user']
-
-            async_to_sync(self.channel_layer.group_send)(
-                user,
-                {
-                    'type': 'ICEcandidate',
-                    'data': {
-                        'rtcMessage': text_data_json['data']['rtcMessage']
-                    }
-                }
-            )
-
-    def call_received(self, event):
-
-        # print(event)
-        print('Call received by ', self.my_name )
-        self.send(text_data=json.dumps({
-            'type': 'call_received',
-            'data': event['data']
+        # Send a welcome message to the connected user
+        await self.send(text_data=json.dumps({
+            'type': 'join_room',
+            'username': username,
         }))
 
+    async def offer(self, event):
+        offer = event['offer']
 
-    def call_answered(self, event):
-
-        # print(event)
-        print(self.my_name, "'s call answered")
-        self.send(text_data=json.dumps({
-            'type': 'call_answered',
-            'data': event['data']
+        # Send the offer to all other users in the room
+        await self.send(text_data=json.dumps({
+            'type': 'offer',
+            'offer': offer,
         }))
 
+    async def answer(self, event):
+        answer = event['answer']
 
-    def ICEcandidate(self, event):
-        self.send(text_data=json.dumps({
-            'type': 'ICEcandidate',
-            'data': event['data']
+        # Send the answer to all other users in the room
+        await self.send(text_data=json.dumps({
+            'type': 'answer',
+            'answer': answer,
+        }))
+
+    async def ice_candidate(self, event):
+        candidate = event['candidate']
+
+        # Send the ICE candidate to all other users in the room
+        await self.send(text_data=json.dumps({
+            'type': 'ice_candidate',
+            'candidate': candidate,
         }))
